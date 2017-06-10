87b842c946ef34b8056459b1f28f86f4c72322a5
dario-ljubic <dljubic@gmail.com>
2017-06-10 19:28:59 +0200

    Fixed parser

--- src/gazetool_filtering.cpp
+++ src/gazetool_filtering.cpp
@@ -2,7 +2,8 @@
 
 //WARNING Sometimes, when running the node, segmentation fault (core dumped) appears due to
 // still unknown reason/s. It is presumed that the implementation with queues is causing the
-// problem. Therefore, implementation of the lists instead of queues should be considered.
+// problem.
+// EDIT: It seems that the problem is fixed, but more testing is needed to be sure!
 
 int main(int argc, char **argv){
     
@@ -12,69 +13,57 @@ int main(int argc, char **argv){
     char *path;
     writeFiltData writeObj;
     char option;
-    
-//    std::cout << argc << std::endl;
-//    std::cout << *argv[0] << std::endl;
-//    std::cout << *argv[1] << std::endl;
-//    std::cout << *argv[2] << std::endl;
-//    std::cout << *argv[3] << std::endl;
-//     
+     
     if (argc == 1) write = false;
     else if (argc > 1) {
         if (argc < 3){
             std::cerr << "Not enough input parameters!" << std::endl;
             return 1;
         }
-        else if (argc > 3){
+        if (argc > 3){
             std::cerr << "Too many input parameters!" << std::endl;
             return 1;
         }
-    }
-    else {
-        option = *argv[1];
-        switch(option){
-            case('w'):
-            {
-                write = true;
-                path = argv[2];
-                writeObj = writeFiltData(logFile, path);
-                break;
-            }
-            //TODO: Add more options if needed
-            default:
-            {
-                std::cerr << "Invalid option!" << std::endl;
-                return 1;
-            }
+        if (argc == 3){
+            option = *argv[1];
+            switch(option){
+                case('w'):
+                {
+                    write = true;
+                    path = argv[2];
+                    std::cout << path << std::endl;
+                    writeObj = writeFiltData(logFile, path);
+                    break;
+                }
+                //TODO: Add more options if needed
+                default:
+                {
+                    std::cerr << "Invalid option!" << std::endl;
+                    return 1;
+                }
+            }             
         }
-             
-    }
+    }    
+    
     ros::init(argc, argv, "gazetool_filter");
     
     subscribeAndPublish filterObj;
     filterObj.initialize();
     
-    ros::Rate r(20); //TODO check the rate in which whis this while loop will execute
-
-    //writeFiltData writeObj(logFile, "/home/larics/schunk_ws/src/gazetool_filtering/log/text.csv");
+    filterObj.wait(); // wait untill connection between this subscriber node and gazetool is established!
+    
+    ros::Rate r(20); //TODO: check the rate in which whis this while loop will execute
     
     while (ros::ok()){
         
-        //ros::spinOnce();
-        
         filterObj.gazeFilter();
         
-        //NOTE: frames are for some reasons not written correctly. It takes about 100 frames for it to 
-        // write without having zeros written.
-        
         if (write) writeObj.dumpEst(logFile, filterObj.output);
-        //writeObj.dumpEst(logFile, filterObj.output);
-        
+
         r.sleep();
     }
     
     if (write) writeObj.closeFile(logFile);
-    //logFile.close();
     
     return 0;
 }
